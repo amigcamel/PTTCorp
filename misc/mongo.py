c7fd7f2fe9305f136a708ff5f01ecb4292606dc3
amigcamel <amigcamel@gmail.com>
2018-06-18 01:12:40 +0800

    change mongodb connection info

--- misc/mongo.py
+++ misc/mongo.py
@@ -1,20 +1,15 @@
+import os
+
 from pymongo import MongoClient
 
 
 def connect(db, collection):
-    client = MongoClient('140.112.147.121')
-    client.admin.authenticate('achiii',
-                              'gjoKClmg8eQDF4pKeVXMkTnX7wL/9MVilkavArDouNA=')
+    client = MongoClient(
+        os.environ['MONGODB_HOST'],
+        int(os.environ['MONGODB_PORT']),
+    )
     output = client[db][collection]
     return output
 
 
-mongoDB = connect  # "connect" in PTTCorp is equivalent to "mongoDB" in COPENS
-
-# This is used for accessing mongodb without authentication
-#from pymongo import Connection
-# def connect(db, collection):
-#	C = Connection(host='localhost', port=27017)
-#	output = C[db][collection]
-#	C.close()
-#	return output
+mongoDB = connect
